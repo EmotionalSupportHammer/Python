993e3461f78c15fb762d98a7c6763029f3316168
EmotionalSupportHammer <bhens960@evit.edu>
2025-07-29 12:36:06 -0700

    Create MY_First_program.py

---
+++ MY_First_program.py
@@ -0,0 +1,16 @@
+import time as t
+
+print("hi")
+def dooms_day_clock():
+    b = 1
+    for x in range (27):
+        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!")
+        b = b+1
+        if b == (28):
+            print("I steped on a lego")
+            
+        else:
+            print("AAAAAAAAAAAAA")
+t.sleep(5)
+
+    
